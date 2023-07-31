b8023eaab26a6c9c0ae899b52bad389576ae3dc1
impact <Impact@fanruan.com>
2023-07-31 22:40:25 +0800

    feat: 提交代码

--- src/index.test.ts
+++ src/index.test.ts
@@ -1,5 +1,91 @@
 import { isEquals } from ".";
 
-it("测试用例", () => {
-  expect(isEquals(1, 1)).toBe(true);
+describe("isEquals", () => {
+  const number = 1;
+  const string = "string";
+  const boolean = true;
+  const bigInt = BigInt(1);
+  const symbol = Symbol(1);
+  const set = new Set();
+  const map = new Map();
+
+  it("参数类型不同不相等", () => {
+    expect(isEquals(number, string)).toBe(false);
+    expect(isEquals(number, boolean)).toBe(false);
+    expect(isEquals(number, null)).toBe(false);
+    expect(isEquals(number, undefined)).toBe(false);
+    expect(isEquals(number, {})).toBe(false);
+    expect(isEquals(number, bigInt)).toBe(false);
+    expect(isEquals(number, symbol)).toBe(false);
+
+    expect(isEquals(string, boolean)).toBe(false);
+    expect(isEquals(string, null)).toBe(false);
+    expect(isEquals(string, undefined)).toBe(false);
+    expect(isEquals(string, {})).toBe(false);
+    expect(isEquals(string, bigInt)).toBe(false);
+    expect(isEquals(string, symbol)).toBe(false);
+
+    expect(isEquals(boolean, null)).toBe(false);
+    expect(isEquals(boolean, undefined)).toBe(false);
+    expect(isEquals(boolean, {})).toBe(false);
+    expect(isEquals(boolean, bigInt)).toBe(false);
+    expect(isEquals(boolean, symbol)).toBe(false);
+
+    expect(isEquals(null, undefined)).toBe(false);
+    expect(isEquals(null, {})).toBe(false);
+    expect(isEquals(null, bigInt)).toBe(false);
+    expect(isEquals(null, symbol)).toBe(false);
+
+    expect(isEquals(undefined, {})).toBe(false);
+    expect(isEquals(undefined, bigInt)).toBe(false);
+    expect(isEquals(undefined, symbol)).toBe(false);
+
+    expect(isEquals({}, bigInt)).toBe(false);
+    expect(isEquals({}, symbol)).toBe(false);
+
+    expect(isEquals(bigInt, symbol)).toBe(false);
+    expect(isEquals(map, set)).toBe(false);
+  });
+
+  it("参数是基本类型并且值相等则相等", () => {
+    expect(isEquals("1", 1)).toBe(false);
+    expect(isEquals(1, 2)).toBe(false);
+    expect(isEquals("1", "2")).toBe(false);
+
+    expect(isEquals(1, 1)).toBe(true);
+    expect(isEquals("1", "1")).toBe(true);
+    expect(isEquals(null, null)).toBe(true);
+    expect(isEquals(true, true)).toBe(true);
+    expect(isEquals(undefined, undefined)).toBe(true);
+    expect(isEquals(BigInt(1), BigInt(1))).toBe(true);
+    expect(isEquals(Symbol(1), Symbol(1))).toBe(false);
+  });
+
+  it("NaN 与 NaN 相等", () => {
+    expect(isEquals(NaN, NaN)).toBe(true);
+  });
+
+  it("+0 与 -0 相等", () => {
+    expect(isEquals(+0, -0)).toBe(true);
+  });
+
+  it("两个对象所有字段相等则相等", () => {
+    expect(isEquals({}, {})).toBe(true);
+    expect(isEquals([], [])).toBe(true);
+    expect(isEquals([1, 2], [1, 2])).toBe(true);
+    expect(isEquals({ a: 1 }, { a: 1 })).toBe(true);
+    expect(isEquals({ a: 1, b: 2 }, { a: 1, b: 2 })).toBe(true);
+    expect(isEquals(new Date(1), new Date(1))).toBe(true);
+    expect(isEquals(new Date(), new Date())).toBe(true);
+  });
+
+  it("两个对象字段不同则不相等", () => {
+    expect(isEquals([1, 2], [1, 3])).toBe(false);
+    expect(isEquals({ a: 1 }, { a: 2 })).toBe(false);
+    expect(isEquals({ a: 1, b: 2 }, { a: 1 })).toBe(false);
+  });
+
+  it("null 和 undefined 不相等", () => {
+    expect(isEquals(null, undefined)).toBe(false);
+  });
 });
--- src/index.ts
+++ src/index.ts
@@ -1,4 +1,33 @@
+function getType(value: any): string {
+  return Object.prototype.toString.call(value);
+}
+
 // eslint-disable-next-line @typescript-eslint/no-explicit-any
 export function isEquals(a: any, b: any): boolean {
+  const typeA = getType(a);
+  const typeB = getType(b);
+
+  if (typeA !== typeB) {
+    return false;
+  }
+  if (a === null || b === null) {
+    return a === b;
+  }
+  // https://developer.mozilla.org/zh-CN/docs/Web/JavaScript/Equality_comparisons_and_sameness
+  if (typeA === "[object Number]") {
+    return a === b || (a !== a && b !== b);
+  }
+  if (typeA === "[object Object]" || typeA === "[object Array]") {
+    for (const key in a) {
+      if (!isEquals(a[key], b[key])) {
+        return false;
+      }
+    }
+
+    return true;
+  }
+  if (typeA === "[object Date]") {
+    return a.getTime() === b.getTime();
+  }
   return a === b;
 }
