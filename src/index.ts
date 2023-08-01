2289ccd5ddafd2f80355b6d22036b07323d3f635
impact <Impact@fanruan.com>
2023-08-01 11:08:23 +0800

    refactor: 优化下代码

--- src/index.ts
+++ src/index.ts
@@ -2,32 +2,46 @@ function getType(value: any): string {
   return Object.prototype.toString.call(value);
 }
 
+function isObjectEqual(
+  a: Record<string, any>,
+  b: Record<string, any>
+): boolean {
+  const keysA = Object.keys(a);
+  const keysB = Object.keys(b);
+
+  if (keysA.length !== keysB.length) {
+    return false;
+  }
+  for (const key of keysA) {
+    if (!b.hasOwnProperty(key)) {
+      return false;
+    }
+    if (!isEquals(a[key], b[key])) {
+      return false;
+    }
+  }
+
+  return true;
+}
+
 // eslint-disable-next-line @typescript-eslint/no-explicit-any
 export function isEquals(a: any, b: any): boolean {
+  if (a === b) return true;
   const typeA = getType(a);
   const typeB = getType(b);
 
-  if (typeA !== typeB) {
-    return false;
-  }
-  if (a === null || b === null) {
-    return a === b;
-  }
-  // https://developer.mozilla.org/zh-CN/docs/Web/JavaScript/Equality_comparisons_and_sameness
-  if (typeA === "[object Number]") {
-    return a === b || (a !== a && b !== b);
-  }
-  if (typeA === "[object Object]" || typeA === "[object Array]") {
-    for (const key in a) {
-      if (!isEquals(a[key], b[key])) {
-        return false;
-      }
-    }
+  if (typeA !== typeB) return false;
 
-    return true;
+  switch (typeA) {
+    case "[object Number]":
+      return a === b || (a !== a && b !== b);
+    case "[object Object]":
+      return isObjectEqual(a, b);
+    case "[object Array]":
+      return isObjectEqual(a, b);
+    case "[object Date]":
+      return a.getTime() === b.getTime();
+    default:
+      return false;
   }
-  if (typeA === "[object Date]") {
-    return a.getTime() === b.getTime();
-  }
-  return a === b;
 }
